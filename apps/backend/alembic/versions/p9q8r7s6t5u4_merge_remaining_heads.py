27d71b2acebc6a52fad2ed9561eb7b0d1e12d22b
Your Name <you@example.com>
2026-03-13 18:05:44 +0530

    fixed:stag

--- apps/backend/alembic/versions/p9q8r7s6t5u4_merge_remaining_heads.py
+++ apps/backend/alembic/versions/p9q8r7s6t5u4_merge_remaining_heads.py
@@ -1,7 +1,7 @@
 """merge remaining heads
 
 Revision ID: p9q8r7s6t5u4
-Revises: c3d4e5f6a7b8, c8d9e0f1a2b3, h3i4j5k6l7m8, h4d5e6f7a8b9, w9x0y1z2a3b4
+Revises: c8d9e0f1a2b3, h4d5e6f7a8b9
 Create Date: 2026-03-13 16:45:00.000000
 
 """
@@ -11,11 +11,8 @@ from typing import Sequence, Union
 
 revision: str = "p9q8r7s6t5u4"
 down_revision: Union[str, Sequence[str], None] = (
-    "c3d4e5f6a7b8",
     "c8d9e0f1a2b3",
-    "h3i4j5k6l7m8",
     "h4d5e6f7a8b9",
-    "w9x0y1z2a3b4",
 )
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
